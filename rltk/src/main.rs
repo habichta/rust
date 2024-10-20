fdbd22517a8c4e44b9bab3d158f36a951a002dee
Arthur Habicht <arthur.habicht@helpany.com>
2024-10-20 15:23:27 +0200

    Working LeftMover

--- rltk/src/main.rs
+++ rltk/src/main.rs
@@ -38,9 +38,19 @@ struct State {
     ecs: World,
 }
 
+impl State {
+    fn run_systems(&mut self) {
+        let mut lw = LeftWalker {};
+        lw.run_now(&self.ecs);
+        self.ecs.maintain();
+    }
+}
+
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
+
+        self.run_systems();
 
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
@@ -52,12 +62,11 @@ impl GameState for State {
 
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
-    let context = RltkBuilder::simple80x50()
-        .with_title("Roguelike Tutorial")
-        .build()?;
+    let context = RltkBuilder::simple80x50().with_title("Fun stuff").build()?;
     let mut gs = State { ecs: World::new() };
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
+    gs.ecs.register::<LeftMover>();
     gs.ecs
         .create_entity()
         .with(Position { x: 40, y: 25 })
@@ -66,7 +75,6 @@ fn main() -> rltk::BError {
             fg: RGB::named(rltk::YELLOW),
             bg: RGB::named(rltk::BLACK),
         })
-        .with(LeftMover {})
         .build();
 
     for i in 0..10 {
@@ -78,6 +86,7 @@ fn main() -> rltk::BError {
                 fg: RGB::named(rltk::RED),
                 bg: RGB::named(rltk::BLACK),
             })
+            .with(LeftMover {})
             .build();
     }
     rltk::main_loop(context, gs)
