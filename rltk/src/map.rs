7e7f645aaa4b980e5d8d397cad7fd25b4e38e632
Arthur Habicht <arthur.habicht@helpany.com>
2024-11-07 23:35:20 +0100

    Connect room

--- rltk/src/map.rs
+++ rltk/src/map.rs
@@ -47,7 +47,7 @@ pub fn xy_idx(x: i32, y: i32) -> usize {
     (y as usize * 80) + x as usize
 }
 
-pub fn new_map_rooms_and_corridors() -> Vec<TileType> {
+pub fn new_map_rooms_and_corridors() -> (Vec<Rect>, Vec<TileType>) {
     let mut map = vec![TileType::Wall; 80 * 50];
 
     let mut rooms: Vec<Rect> = Vec::new();
@@ -71,11 +71,22 @@ pub fn new_map_rooms_and_corridors() -> Vec<TileType> {
         }
         if ok {
             apply_room_to_map(&new_room, &mut map);
+            if !rooms.is_empty() {
+                let (new_x, new_y) = new_room.center();
+                let (prev_x, prev_y) = rooms[rooms.len() - 1].center();
+                if rng.range(0, 2) == 1 {
+                    apply_horizontal_tunnel(&mut map, prev_x, new_x, prev_y);
+                    apply_vertical_tunnel(&mut map, prev_y, new_y, new_x);
+                } else {
+                    apply_vertical_tunnel(&mut map, prev_y, new_y, prev_x);
+                    apply_horizontal_tunnel(&mut map, prev_x, new_x, new_y);
+                }
+            }
             rooms.push(new_room);
         }
     }
 
-    map
+    (rooms, map)
 }
 fn apply_horizontal_tunnel(map: &mut [TileType], x1: i32, x2: i32, y: i32) {
     for x in min(x1, x2)..=max(x1, x2) {
